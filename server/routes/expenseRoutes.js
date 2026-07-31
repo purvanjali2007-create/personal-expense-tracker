027af48135eb12ac2aa4ff725089e6e276eb9fa0
arunashinde <arushi_822004@yahoo.co.in>
2026-07-31 19:04:27 +0530

    Fixed expense routes

--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -1,24 +1,23 @@
 const express = require("express");
-
 const router = express.Router();
 
 const {
-    addExpense,
-    getExpenses,
-    deleteExpense,
-    updateExpense
+  addExpense,
+  getExpenses,
+  deleteExpense,
+  updateExpense,
 } = require("../controllers/expenseController");
 
-router.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Route Working"
-    });
-});
+// Get all expenses
+router.get("/", getExpenses);
+
+// Add expense
 router.post("/", addExpense);
 
-router.get("/", getExpenses);
-router.put("/:id", updateExpense)
+// Update expense
+router.put("/:id", updateExpense);
+
+// Delete expense
 router.delete("/:id", deleteExpense);
 
 module.exports = router;
