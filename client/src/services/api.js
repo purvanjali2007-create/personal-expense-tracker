f80fdb06d2a697f53b49e7df384dab5068df2dd2
arunashinde <arushi_822004@yahoo.co.in>
2026-07-31 15:48:34 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://personal-expense-tracker-1.onrender.com/api"
 });
 
+/* const API = axios.create({
+  baseURL: "http://localhost:5000/api" https://personal-expense-tracker-1.onrender.com/api
+});
+ */
 export default API;
