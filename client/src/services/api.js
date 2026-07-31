b02b275d28f884902beef514cd7c3e1e91609be5
arunashinde <arushi_822004@yahoo.co.in>
2026-07-31 16:58:10 +0530

    remove duplicate app var

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
+const api = axios.create({
   baseURL: "https://personal-expense-tracker-1.onrender.com/api/expenses"
 });
+
+export default api;
